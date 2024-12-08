484711bf125fb14d8d0637b6849d94295aa1e943
yevheniiKolisnykSAN <92385@student.san.edu.pl>
2024-12-08 09:28:19 +0100

    product-card

---
+++ src/app/pages/catalog/catalog-card/catalog-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-catalog-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './catalog-card.component.html',
+  styleUrl: './catalog-card.component.scss'
+})
+export class CatalogCardComponent {
+
+}
---
+++ src/app/shared/pipes/cut-text.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'cutText',
+  standalone: true
+})
+export class CutTextPipe implements PipeTransform {
+
+  transform(value: unknown, ...args: unknown[]): unknown {
+    return null;
+  }
+
+}
